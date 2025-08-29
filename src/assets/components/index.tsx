eaec9758611d703aea95358d1f7dc5bd986fe0ab
Oleg Voloschuk <t0687538782@gmail.com>
2025-08-29 16:38:36 +0300

    init commit

--- src/assets/components/index.tsx
+++ src/assets/components/index.tsx
@@ -1,1 +1,19 @@
 import './style.css'
+import { Site } from '../../../type/mainPage'
+
+
+const Site = (props:Site) =>{
+
+
+  return (
+    <>
+      <Тіло компоненту / HTML>
+    </>
+  )
+}
+
+
+export default Site
+
+
+
